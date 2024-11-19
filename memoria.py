b29f3905ba0bca3840e721ff608f951ac94ffec5
Roger Santos Bezerra <166184482+rogerin07@users.noreply.github.com>
2024-11-18 23:10:34 -0300

    Update memoria.py

--- memoria.py
+++ memoria.py
@@ -1,16 +1,16 @@
 import tkinter as tk
 from tkinter import messagebox
+import random
 
 class Memoria:
     def __init__(self, total_memoria):
+        self.total_memoria = total_memoria
         self.blocos = [(0, total_memoria, False)]  
 
     def alocar_memoria(self, processo_id, tamanho):
         for i, (inicio, tamanho_bloco, ocupado) in enumerate(self.blocos):
             if not ocupado and tamanho_bloco >= tamanho:
-          
                 self.blocos[i] = (inicio, tamanho, True, processo_id)
-                
                 if tamanho_bloco > tamanho:
                     self.blocos.insert(i + 1, (inicio + tamanho, tamanho_bloco - tamanho, False))
                 return True
@@ -19,10 +19,7 @@ class Memoria:
     def desalocar_memoria(self, processo_id):
         for i, bloco in enumerate(self.blocos):
             if len(bloco) == 4 and bloco[3] == processo_id:
-                
                 self.blocos[i] = (bloco[0], bloco[1], False)
-                
-                
                 self._consolidar_blocos()
                 return True
         return False
@@ -93,7 +90,7 @@ class InterfaceMemoria(tk.Tk):
         y = 10
         largura = 280
         
-        for inicio, tamanho, ocupado, *resto in self.memoria.blocos:
+        for _, tamanho, ocupado, *resto in self.memoria.blocos:
             altura = (tamanho / self.memoria.total_memoria) * 280  
             cor = "red" if ocupado else "green"
             processo_id = resto[0] if ocupado else ""
